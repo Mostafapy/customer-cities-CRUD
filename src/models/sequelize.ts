0cf8a9d074b4071d4208e271cca537fcc65b89f6
Mostafa Hisham <mmham3919@gmail.com>
2021-05-29 17:54:42 +0200

    adding sequelize configuration file

---
+++ src/models/sequelize.ts
@@ -0,0 +1,37 @@
+import { Sequelize } from 'sequelize-typescript';
+
+// Import Models
+import City from './city.model';
+import Customer from './customer.model';
+
+const sequelize: Sequelize = new Sequelize(
+  process.env.DATABASE_DB_NAME || '',
+  process.env.DATABASE_USER_NAME || '',
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT || ''),
+    dialect: 'mysql',
+    dialectOptions: { decimalNumbers: true },
+    pool: {
+      max: 30,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+      timestamps: true,
+      freezeTableName: false,
+    },
+    // models: [join(__dirname, './*.model.js')],
+    logging: false,
+  },
+);
+
+sequelize.addModels([
+  City,
+  Customer
+]);
+export default sequelize;
