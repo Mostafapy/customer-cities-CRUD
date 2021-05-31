a2bef2e1a8ebe10b6eebcba34284b589a9e6e920
Mostafa Hisham <mmham3919@gmail.com>
2021-05-31 16:11:17 +0200

    adding searchForCustomer in customer controller

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -4,49 +4,69 @@ import * as customerService from '../services/customer.service';
 
 // [POST] /api/v1/customer/
 const addCustomer: RequestHandler = async (req: Request, res: Response) => {
-      const { name }: CustomerDtos.CustomerDto = req.body;
+  const { name }: CustomerDtos.CustomerDto = req.body;
 
-     const response = await customerService.addCustomer(name);
+  const response = await customerService.addCustomer(name);
 
-     return res.status(response.statusCode).json(response.body);
+  return res.status(response.statusCode).json(response.body);
 };
 
 // [PUT] /api/v1/customer/:id
 const editCustomer: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const { name }: CustomerDtos.CustomerDto = req.body;
+  const { name }: CustomerDtos.CustomerDto = req.body;
 
-   const response = await customerService.editCustomer({ id: parseInt(id), name });
+  const response = await customerService.editCustomer({
+    id: parseInt(id),
+    name,
+  });
 
-   return res.status(response.statusCode).json(response.body);
+  return res.status(response.statusCode).json(response.body);
 };
 
 // [GET] /api/v1/customer?id=
-const findCustomerById: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.query;
+const findCustomerById: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
 
-    const requiredId = id?.toString() || '0';
+  const response = await customerService.findCustomerById(parseInt(id));
 
-    const response = await customerService.findCustomerById(parseInt(requiredId));
-
-    return res.status(response.statusCode).json(response.body);
+  return res.status(response.statusCode).json(response.body);
 };
 
 // [DELETE] /api/v1/customer?id=
-const deleteCustomerById: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.query;
+const deleteCustomerById: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
 
-    const requiredId = id?.toString() || '0';
+  const response = await customerService.findCustomerById(parseInt(id));
 
-    const response = await customerService.findCustomerById(parseInt(requiredId));
+  return res.status(response.statusCode).json(response.body);
+};
 
-    return res.status(response.statusCode).json(response.body);
+// [GET] /api/v1/customer/search?name=
+const searchForCustomer: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { name } = req.query;
+
+  const requiredName = name?.toString() || '';
+
+  const response = await customerService.searchForCustomer(requiredName);
+
+  return res.status(response.statusCode).json(response.body);
 };
 
 export {
-    addCustomer,
-    editCustomer,
-    findCustomerById,
-    deleteCustomerById
+  addCustomer,
+  editCustomer,
+  findCustomerById,
+  deleteCustomerById,
+  searchForCustomer,
 };
