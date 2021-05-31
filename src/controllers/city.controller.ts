d9782b70d31db012f223ce9083d5ba304337d7cf
Mostafa Hisham <mmham3919@gmail.com>
2021-05-31 16:08:04 +0200

    adding searchForCity controller in city controller

--- src/controllers/city.controller.ts
+++ src/controllers/city.controller.ts
@@ -4,45 +4,51 @@ import * as cityService from '../services/city.service';
 
 // [POST] /api/v1/city/
 const addCity: RequestHandler = async (req: Request, res: Response) => {
-      const { name }: CityDtos.CityDto = req.body;
+  const { name }: CityDtos.CityDto = req.body;
 
-     const response = await cityService.addCity(name);
+  const response = await cityService.addCity(name);
 
-     return res.status(response.statusCode).json(response.body);
+  return res.status(response.statusCode).json(response.body);
 };
 
 // [PUT] /api/v1/city/:id
 const editCity: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const { name }: CityDtos.CityDto = req.body;
+  const { name }: CityDtos.CityDto = req.body;
 
-   const response = await cityService.editCity({ id: parseInt(id), name });
+  const response = await cityService.editCity({ id: parseInt(id), name });
 
-   return res.status(response.statusCode).json(response.body);
+  return res.status(response.statusCode).json(response.body);
 };
 
 // [GET] /api/v1/city/:id=
 const findCityById: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const response = await cityService.findCityById(parseInt(id));
+  const response = await cityService.findCityById(parseInt(id));
 
-    return res.status(response.statusCode).json(response.body);
+  return res.status(response.statusCode).json(response.body);
 };
 
 // [DELETE] /api/v1/city/:id
 const deleteCityById: RequestHandler = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const response = await cityService.findCityById(parseInt(id));
+  const response = await cityService.findCityById(parseInt(id));
 
-    return res.status(response.statusCode).json(response.body);
+  return res.status(response.statusCode).json(response.body);
 };
 
-export {
-    addCity,
-    editCity,
-    findCityById,
-    deleteCityById
+// [GET] /api/v1/city/search?name=
+const searchForCity: RequestHandler = async (req: Request, res: Response) => {
+  const { name } = req.query;
+
+  const requiredName = name?.toString() || '';
+
+  const response = await cityService.searchForCity(requiredName);
+
+  return res.status(response.statusCode).json(response.body);
 };
+
+export { addCity, editCity, findCityById, deleteCityById, searchForCity };
