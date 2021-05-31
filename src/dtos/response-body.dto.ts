a79e5bd973b22a437d3f8e7300cdb4fef432aac2
Mostafa Hisham <mmham3919@gmail.com>
2021-05-31 16:26:25 +0200

    fix ResponseBodyDto to validationErros

--- src/dtos/response-body.dto.ts
+++ src/dtos/response-body.dto.ts
@@ -3,7 +3,7 @@ import { EMessages } from '../enums/messages.enum';
 export class ResponseBodyDto<T> {
     public message: EMessages;
     public error?: string;
-    public validationError?: string;
+    public validationErrors?: any[];
     public success: boolean;
     public data: T | any;
 }
