e7966471f210eeccb526fa44d67ca9bbfff7cc4a
Mostafa Hisham <mmham3919@gmail.com>
2021-05-31 23:21:41 +0200

    adding validation middlewares to city and customer routes

--- src/routes/city.route.ts
+++ src/routes/city.route.ts
@@ -1,18 +1,43 @@
 import { Router } from 'express';
+import validationMiddleware from '../middlewares/validation.middleware';
 import * as controller from '../controllers/city.controller';
+import * as cityValidationSchemas from '../validations/schemas/city/index';
 
 // [USE] /api/v1/city
 const router: Router = Router();
 
-router.post('/', controller.addCity);
+router.post(
+  '/',
+  ...cityValidationSchemas.addCitySchema,
+  validationMiddleware(),
+  controller.addCity
+);
 
-router.put('/:id', controller.editCity);
+router.put(
+  '/:id',
+  validationMiddleware(),
+  controller.editCity
+);
 
-router.get('/:id', controller.findCityById);
+router.get(
+  '/:id',
+  ...cityValidationSchemas.findCityByIdSchema,
+  validationMiddleware(),
+  controller.findCityById
+);
 
-router.delete('/:id', controller.deleteCityById);
+router.delete(
+  '/:id',
+  ...cityValidationSchemas.deleteCityByIdSchema,
+  validationMiddleware(),
+  controller.deleteCityById
+);
 
-router.get('/search', controller.searchForCity);
+router.get(
+  '/search',
+  ...cityValidationSchemas.searchForCitySchema,
+  validationMiddleware(),
+  controller.searchForCity
+);
 
 export default router;
-
--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -1,17 +1,44 @@
 import { Router } from 'express';
+import validationMiddleware from '../middlewares/validation.middleware';
 import * as controller from '../controllers/customer.controller';
+import * as customerValidationSchemas from '../validations/schemas/customer/index';
 
 // [USE] /api/v1/customer
 const router: Router = Router();
 
-router.post('/', controller.addCustomer);
+router.post(
+  '/',
+  ...customerValidationSchemas.addCustomerSchema,
+  validationMiddleware(),
+  controller.addCustomer
+);
 
-router.put('/:id', controller.editCustomer);
+router.put(
+  '/:id',
+  ...customerValidationSchemas.editCustomerSchema,
+  validationMiddleware(),
+  controller.editCustomer
+);
 
-router.get('/:id', controller.findCustomerById);
+router.get(
+  '/:id',
+  ...customerValidationSchemas.findCustomerByIdSchema,
+  validationMiddleware(),
+  controller.findCustomerById
+);
 
-router.delete('/:id', controller.deleteCustomerById);
+router.delete(
+  '/:id',
+  ...customerValidationSchemas.deleteCustomerByIdSchema,
+  validationMiddleware(),
+  controller.deleteCustomerById
+);
 
-router.get('/search', controller.searchForCustomer);
+router.get(
+  '/search',
+  ...customerValidationSchemas.searchForCustomerSchema,
+  validationMiddleware(),
+  controller.searchForCustomer
+);
 
 export default router;
