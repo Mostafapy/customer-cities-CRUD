b8a87b352c898dbd8eaa248525d2a1f6da10b462
Mostafa Hisham <mmham3919@gmail.com>
2021-05-31 23:18:20 +0200

    fix validation middleware

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
-import { validationResult } from 'express-validator';
+import { ValidationError, validationResult } from 'express-validator';
 import { EMessages } from '../enums/messages.enum';
 import { Logger } from '../utils/logger.utils';
 
 const logger = new Logger('Validation middleware');
+
+const errorFormatter = (obj: ValidationError) => {
+  // Build your resulting errors however you want! String, object, whatever - it works!
+  return `${obj.location}[${obj.param}]: ${obj.msg}`;
+};
 
 /**
  * Middleware to validation request using express-validator package
@@ -11,23 +16,23 @@ const logger = new Logger('Validation middleware');
 
 const validationMiddleware = (): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
+      const errors = validationResult(req).formatWith(errorFormatter);
 
-    if (!errors.isEmpty()) {
-      logger.error('validationMiddleware Error', { err: new Error('Input validation Errors') });
-
-      return res.status(422).json({
-        message: EMessages.serverError,
-        error: 'Validation Error',
-        validationErrors: errors,
-        success: false,
-        data: null,
-      });
-    }
-
-    next();
+      if (!errors.isEmpty()) {
+        logger.error('validationMiddleware Error', {
+          err: new Error('Input validation Errors'),
+        });
+  
+        return res.status(422).json({
+          message: EMessages.serverError,
+          error: 'Validation Error',
+          validationErrors: errors.array(),
+          success: false,
+          data: null,
+        });
+      }
+      next();
   };
 };
-
 
 export default validationMiddleware;
