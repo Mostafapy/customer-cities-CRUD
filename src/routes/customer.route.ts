7c3cf753e10ba492e01293cf88bb46f39144642c
Mostafa Hisham <mmham3919@gmail.com>
2021-05-31 11:48:19 +0200

    adding customer route file

---
+++ src/routes/customer.route.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import * as controller from '../controllers/customer.controller';
+
+// [USE] /api/v1/customer
+const router: Router = Router();
+
+router.post('/', controller.addCustomer);
+
+router.put('/:id', controller.editCustomer);
+
+router.get('/:id', controller.findCustomerById);
+
+router.delete('/:id', controller.deleteCustomerById);
+
+export default router;
